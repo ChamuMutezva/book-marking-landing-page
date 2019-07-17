26ce6f6f376386d9f39b3121635c4bcb8bf1f7d5
Chamunorwa Mutezva <ckmutezva@gmail.com>
2019-07-17 22:11:54 +0200

    modal fix scroll removed on modal

--- main.js
+++ main.js
@@ -8,6 +8,29 @@ for (let opened of openClose) {
     opened.addEventListener("click", function () {
         console.log(this);
         modal.classList.toggle("show");
-
+        openModal();
     })
+}
+
+/*$('#modal').on('shown', function () {
+    $('body').css('overflow', 'hidden');
+}).on('hidden', function () {
+    $('body').css('overflow', 'auto');
+})*/
+
+function openModal() {
+    if (modal.classList.contains("show")) {
+        document.body.style.overflow = "auto";
+      
+       // const top = document.body.style.top;
+        //document.body.style.position = 'fixed';
+       // document.body.style.top = `-${window.scrollY}px`;
+        //window.scrollTo(0, parseInt(scrollY || '0') * -1);
+        console.log("modal ent 1");
+    } else {
+        document.body.style.overflow = "hidden";
+        //document.body.style.position = '';
+        //document.body.style.top = '';
+        console.log("modal ent 3");
+    }
 }
