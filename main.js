dbf447f7739d015ca9e435e9035d65df415802d4
Chamunorwa Mutezva <ckmutezva@gmail.com>
2019-07-19 19:59:55 +0200

    altenate the long way

--- main.js
+++ main.js
@@ -34,6 +34,37 @@ function openModal() {
         console.log("modal ent 3");
     }
 }
+
+/*
+const altenateLinks = Array.from(document.querySelectorAll(".featureLinks"));
+//console.log(altenateLinks);
+altenateLinks.forEach(elem => {
+    
+    elem.addEventListener("click", function (evt) {
+        const clickedLink = evt.target; 
+        const changeTheseTargets = Array.from(document.querySelectorAll(".targets"));
+        if (clickedLink) {
+            const selectTarget = this.classList.item(this.classList.length - 1);
+            //console.log(selectTarget);
+            //console.log(changeTheseTargets);
+            changeTheseTargets.forEach(elements => {
+                if (elements.classList.contains(selectTarget)) {
+                    console.log(this);
+                    console.log(elements);
+                   elements.style.marginTop = "10%";
+                   elements.style.height = "100%";
+                    elements.style.visibility = "visible";
+                }
+                if (!elements.classList.contains(selectTarget)) {
+                    elements.style.marginTop = "0";
+                    elements.style.height = "0";
+                    elements.style.visibility = "hidden";
+                }
+            })
+        }
+    })
+})
+*/
 function openAltImgSearch() {
     let target1 = document.querySelector('.altenateImg');
     target1.style.marginTop = "10%";
@@ -75,3 +106,24 @@ function openSpeedySearch() {
 }
 let openLinkSpeedy = document.querySelector(".speedySearchingLink");
 openLinkSpeedy.addEventListener("click", openSpeedySearch);
+
+function easySharing() {
+    let target1 = document.getElementById('shareBkMarks');
+    target1.style.marginTop = "10%";
+    target1.style.height = "100%";
+    target1.style.visibility = "visible";
+
+
+    let closeTarget1 = document.querySelector('.altenateImg');
+    closeTarget1.style.marginTop = "0";
+    closeTarget1.style.height = "0";
+    closeTarget1.style.visibility = "hidden";
+
+    let closeTarget2 = document.getElementById('speedySearch');
+    closeTarget2.style.marginTop = "0";
+    closeTarget2.style.height = "0";
+    closeTarget2.style.visibility = "hidden";
+
+}
+let easySharingBkMarks = document.querySelector(".easySharingLink");
+easySharingBkMarks.addEventListener("click", easySharing);
