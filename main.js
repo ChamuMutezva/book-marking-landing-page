bb71fb535ab014e76e3bf1860a23a411e93168f5
Chamunorwa Mutezva <ckmutezva@gmail.com>
2019-07-26 05:40:55 +0200

    placeholder color changed

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ let modal = document.getElementById("modal");
 console.log(openClose);
 console.log(modal);
 
-
 for (let opened of openClose) {
     opened.addEventListener("click", function () {
         console.log(this);
