babdd4eee2e074b46cde589e9da898be08fb6ecf
Chamunorwa Mutezva <ckmutezva@gmail.com>
2019-07-30 18:22:30 +0200

    JS refactored to use 'forEach function'

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 let openClose = Array.from(document.querySelectorAll(".openClose"));
 let modal = document.getElementById("modal");
+const imageList = ["images/illustration-hero.svg", "images/illustration-features-tab-2.svg", "images/illustration-features-tab-3.svg"];
+const headersList = [`Bookmark in one click`, "Intelligent search", "Share your bookmarks"];
+const paraList = [`Organize your bookmarks however you like. Our simple drag-and-drop interface
+                gives you complete control over how you manage your favourite sites.`,
+    `Our powerful search feature will help you find saved sites in no time at all.
+                No need to trawl through all of your bookmarks.`,
+    `Easily share your bookmarks and collections with others. Create a shareable
+    link that you can send at the click of a button. `
+];
+
 console.log(openClose);
 console.log(modal);
 
@@ -10,12 +20,8 @@ for (let selTar of clickable) {
     selTar.style.borderBottom = "transparent";
     selTar.addEventListener("click", function (evt) {
         clickable.forEach(elem => elem.style.borderBottom = "transparent");
-        console.log(evt.target);        
-       // if (evt.target) {
-            this.style.borderBottom = "1px solid red";
-        //} else if (!evt.target) { 
-        
-   // }
+        console.log(evt.target);
+        this.style.borderBottom = "1px solid red";
     })
 }
 
@@ -23,11 +29,25 @@ for (let selTar of clickable) {
 for (let opened of openClose) {
     opened.addEventListener("click", function () {
         console.log(this);
-        modal.classList.toggle("show");       
-        openModal();      
-       
+        modal.classList.toggle("show");
+        openModal();
+
     })
-}
+};
+
+
+clickable.forEach((elem, index) => {
+    const curImage = document.querySelector('.mainImages img');
+    const curHeading = document.querySelector('.mainImagesDescr h3');
+    const curPara = document.querySelector('.mainImagesDescr p');
+    console.log(elem, index);
+    elem.addEventListener("click", function () {
+        //console.log(this + " i have been clicked. Positioned at " + index);
+        curImage.src = imageList[index];
+        curHeading.innerHTML = headersList[index];
+        curPara.innerHTML = paraList[index];
+    })
+})
 
 
 
@@ -35,18 +55,18 @@ let features = Array.from(document.querySelectorAll(".modalFeatures"));
 for (let feature of features) {
     feature.addEventListener("click", function () {
         console.log(modal.classList);
+
         modal.classList.toggle("show");
         openModal();
         console.log(modal);
 
         let modalHeight = modal.clientHeight;
         console.log(modalHeight);
-        if (modalHeight == 116) {
+        if (modalHeight > 0) {
             document.body.style.overflow = "auto";
         }
     })
-    
-   
+
 }
 /*$('#modal').on('shown', function () {
     $('body').css('overflow', 'hidden');
@@ -54,10 +74,10 @@ for (let feature of features) {
     $('body').css('overflow', 'auto');
 })*/
 
-function openModal() {     
-    
-    if (modal.classList.contains("show")) {        
-        document.body.style.overflow = "auto";       
+function openModal() {
+
+    if (modal.classList.contains("show")) {
+        document.body.style.overflow = "auto";
         // const top = document.body.style.top;
         //document.body.style.position = 'fixed';
         // document.body.style.top = `-${window.scrollY}px`;
@@ -65,13 +85,14 @@ function openModal() {
         console.log("modal ent 1");
     } else {
         document.body.style.overflow = "hidden";
-       
+
         //document.body.style.position = '';
         //document.body.style.top = '';
         console.log("modal ent 3");
     }
 }
 
+/*link number one
 function oneClickBookmark() {
     let curImage = document.querySelector('.mainImages img');
     let curHeading = document.querySelector('.mainImagesDescr h3');
@@ -85,7 +106,7 @@ function oneClickBookmark() {
 let openAltImgLink = document.querySelector(".simpleBookMarkingLink");
 openAltImgLink.addEventListener("click", oneClickBookmark);
 
-
+//link number 2
 function speedSearching() {
     let curImage = document.querySelector('.mainImages img');
     let curHeading = document.querySelector('.mainImagesDescr h3');
@@ -99,6 +120,7 @@ function speedSearching() {
 let openLinkSpeedy = document.querySelector(".speedySearchingLink");
 openLinkSpeedy.addEventListener("click", speedSearching);
 
+//link number 3
 function toggleBookMark() {
     let curImage = document.querySelector('.mainImages img');
     let curHeading = document.querySelector('.mainImagesDescr h3');
@@ -112,4 +134,8 @@ function toggleBookMark() {
 
 let easySharingBkMarks = document.querySelector(".easySharingLink");
 easySharingBkMarks.addEventListener("click", toggleBookMark);
+*/
+
+
+
 
